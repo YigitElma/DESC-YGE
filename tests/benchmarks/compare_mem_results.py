3c352072a231e5b1f445217a32d0a4944f0f26fe
YigitElma <yigitelmacioglu@gmail.com>
2025-04-24 17:34:35 -0400

    update

--- tests/benchmarks/compare_mem_results.py
+++ tests/benchmarks/compare_mem_results.py
@@ -43,10 +43,8 @@ plt.savefig(PNG, dpi=100)
 
 # ---------- commit message ----------
 msg = f"### Memory benchmark result\n\n```diff\n"
-msg += (
-    f"| {'Test Name':^22} | {'Master':^22} | {'PR':^22} | {'Δ (MB)':^22} | {'%Δ':^22} |"
-)
-msg += f"\n| {'-'*22} | {'-'*22} | {'-'*22} | {'-'*22} | {'-'*22} |\n"
+msg += f"| {'Test Name':^22} | {'Master':^22} | {'PR':^22} | {'Δ (MB)':^22} | {'%Δ':^22} |\n"
+msg += f"| {'-'*22} | {'-'*22} | {'-'*22} | {'-'*22} | {'-'*22} |\n"
 for i, name in enumerate(data_master.keys()):
     peak_pr = data_pr[name]["mem"].max()
     peak_ma = data_master[name]["mem"].max()
@@ -54,21 +52,21 @@ for i, name in enumerate(data_master.keys()):
     percent_change = (delta / peak_ma) * 100
     sign = "-" if delta >= 0 else "+"
     msg += (
-        f"{sign} {name:^22} | {peak_ma:.1f} | {peak_pr:.1f} |"
-        + f" {sign}{abs(delta):.1f} | {sign}{abs(percent_change):.2f}% |\n"
+        f"{sign} {name:^22} | {peak_ma:^22.1f} | {peak_pr:^22.1f} |"
+        + f" {sign}{abs(delta):^22.1f} | {sign}{abs(percent_change):^22.2f}% |\n"
     )
 msg += f"```"
 
 with open("commit_msg.txt", "w") as fh:
     fh.write(msg)
 
-# # ---------- add the image to the job summary ----------
-# summary = os.getenv("GITHUB_STEP_SUMMARY")
-# if summary:
-#     with open(PNG, "rb") as fh:
-#         b64 = base64.b64encode(fh.read()).decode()
-#     with open(summary, "a") as out:
-#         out.write("\n## Memory timeline\n\n")
-#         out.write(
-#             f'<img src="data:image/png;base64,{b64}" alt="memory plot" width="800"/>\n'
-#         )
+# ---------- add the image to the job summary ----------
+summary = os.getenv("GITHUB_STEP_SUMMARY")
+if summary:
+    with open(PNG, "rb") as fh:
+        b64 = base64.b64encode(fh.read()).decode()
+    with open(summary, "a") as out:
+        out.write("\n## Memory timeline\n\n")
+        out.write(
+            f'<img src="data:image/png;base64,{b64}" alt="memory plot" width="800"/>\n'
+        )
